348c8f6ba681f8cd3a83618ecc931ec58c91b30d
abdulmajeedkhurasani <abdulmajeedkhurasani@outlook.com>
2021-08-08 21:37:21 +0500

    Creating Shaders class

--- OpenGLStarting.cpp
+++ OpenGLStarting.cpp
@@ -3,43 +3,28 @@
 #include <GLFW/glfw3.h>
 #include <iostream>
 #include <math.h>
+#include "Shaders/Shader.h"
+
 // incase of resizing window glViewPort should resize
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);  
 void processInput(GLFWwindow* window);
 
-// vertex shader just passes input to out put 
-const char *vertexShaderSource = 
-        "#version 330 core\n"
-        "layout (location = 0) in vec3 aPos;\n"
-        "layout (location = 1) in vec3 aColor;\n"
-        "out vec3 ourColor;\n"
-        "void main()\n"
-        "{\n"
-        "   gl_Position = vec4(aPos, 1.0);\n"
-        "   ourColor = aColor;\n"
-        "}\0";
-const char *fragmentShaderSource = 
-        "#version 330 core\n"
-        "out vec4 FragColor;\n"
-        "in vec3 ourColor;\n"
-        "void main()\n"
-        "{\n"
-        "   FragColor = vec4(ourColor, 1.0);\n"
-        "}\0";
-
-
+const int width = 800;
+const int height = 600;
 
 int main()
 {
     // all for opening a window can be done with other libraries
-    glfwInit(); // initialize glfw 
+    glfwInit();
     // set version and make it core profile
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR,3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR,3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-    const int width = 800;
-    const int height = 600;
+#ifdef __APPLE__
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+#endif
+
     // creat a winddow 
     GLFWwindow* window = glfwCreateWindow(width, height, "name", NULL, NULL);
     // Check for window if it is created 
@@ -51,6 +36,8 @@ int main()
     }
     //make the context of our window the main context on the current thread
     glfwMakeContextCurrent(window);
+    // register resize function 
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
     //Initialize glad. Glad manages function pointers for openGl
     if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
@@ -58,63 +45,10 @@ int main()
         std::cout << "Failed to initialize GLAD\n";
     }
 
+    // build and compile  shader program
+    // ------------------------------------
+    Shader shaderProgram("Shaders/verShader330", "Shaders/fregShader330"); 
     
-    // register resize function 
-    
-    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
-    
-
-
-
-    
-    //create a shader object 
-    unsigned int vertexShader;
-    vertexShader = glCreateShader(GL_VERTEX_SHADER);
-    //Compiling shader 
-    // shader id, number of strings, shader source . binding shader to shader ogject
-    glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
-    //compile shader 
-    glCompileShader(vertexShader);
-    // check compile time error 
-    int success;
-    char infoLog[512];
-    glGetShaderiv(vertexShader,GL_COMPILE_STATUS, &success);
-    if (!success)
-    {
-        glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
-        std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n"  << infoLog << "\n";
-    }
-    // do same for fragment shader 
-    
-    unsigned int fragmentShader;
-    fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
-    glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
-    glCompileShader(fragmentShader);
-    // compile time error
-    glGetShaderiv(fragmentShader,GL_COMPILE_STATUS, &success);
-    if (!success)
-    {
-        glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
-        std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n"  << infoLog << "\n";
-    }
-
-    // creat a shader program to link them with shaders and have its id reference
-    // uGLuint is an unsigned int 
-    GLuint shaderProgram = glCreateProgram();
-    glAttachShader(shaderProgram, vertexShader);
-    glAttachShader(shaderProgram, fragmentShader);
-    glLinkProgram(shaderProgram);
-    // check for linking errors 
-    glGetProgramiv(shaderProgram,GL_LINK_STATUS, &success);
-    if (!success)
-    {
-        glGetProgramInfoLog(shaderProgram,512,NULL,infoLog);
-        std::cout << "ERROR::SHADER::LINKING::COMPILATION_FAILED\n"  << infoLog << "\n";
-    }
-    // once shader are linked there objects are no longer needed
-    glDeleteShader(vertexShader);
-    glDeleteShader(fragmentShader);
-
     float vertices[] = {
         // vetices            //color
          0.0f,  0.5f, 0.0f, 1.0f, 0.0f, 0.0f, 
@@ -158,7 +92,7 @@ int main()
         // the triangle
 
        // render stuff 
-        glUseProgram(shaderProgram);
+        shaderProgram.use();
 
         glBindVertexArray(VAO);
         glDrawElements(GL_TRIANGLES, 3, GL_UNSIGNED_INT, 0);
@@ -170,6 +104,8 @@ int main()
         glfwSwapBuffers(window);
     }
 
+    glDeleteVertexArrays(1, &VAO);
+    glDeleteBuffers(1, &VBO);
 
     glfwTerminate();
     return 0;
