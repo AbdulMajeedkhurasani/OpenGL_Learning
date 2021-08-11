2c5a89548151d3d91baca91ab15624771d68abbe
abdulmajeedkhurasani <abdulmajeedkhurasani@outlook.com>
2021-08-11 18:58:12 +0500

    Shader class

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-g++ -Wall -o $1 $2 stb_image.cpp glad.c -lGL -lglut  -lglfw3  -lGLU -lGLEW -lglfw3 -lX11 -lXxf86vm -lXrandr -lpthread -lXi -ldl -lXinerama -lXcursor
+g++ -Wall -o $1 $2 stb_image.cpp Shaders/Shader.cpp glad.c -lGL -lglut  -lglfw3  -lGLU -lGLEW -lglfw3 -lX11 -lXxf86vm -lXrandr -lpthread -lXi -ldl -lXinerama -lXcursor
 ./$1
