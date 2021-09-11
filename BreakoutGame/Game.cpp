0a198ec6be0ac83df0c1667300ca445bcb725a93
abdulmajeedkhurasani <abdulmajeedkhurasani@outlook.com>
2021-09-12 02:01:31 +0500

    2D Game paddle

--- BreakoutGame/Game.cpp
+++ BreakoutGame/Game.cpp
@@ -6,6 +6,7 @@
 
 // Game-related State data
 SpriteRenderer  *Renderer;
+GameObject *Player;
 
 
 Game::Game(unsigned int width, unsigned int height) 
@@ -29,7 +30,6 @@ void Game::Init()
     Shader shader = ResourceManager::GetShader("sprite");
     shader.Use().SetInteger("image", 0);
     shader.SetMatrix4("projection", projection);
-
     // set render-specific controls
     Renderer = new SpriteRenderer(shader);
     // load textures
@@ -37,18 +37,20 @@ void Game::Init()
     ResourceManager::LoadTexture("resources/textures/awesomeface.png", true, "face");
     ResourceManager::LoadTexture("resources/textures/block.png", false, "block");
     ResourceManager::LoadTexture("resources/textures/block_solid.png", false, "block_solid");
-
-    //load levels
-
-    GameLevel one;   one.Load  ("resources/levels/one.lvl",   Width, Height / 2);
-    GameLevel two;   two.Load  ("resources/levels/two.lvl",   Width, Height / 2);
-    GameLevel three; three.Load("resources/levels/three.lvl", Width, Height / 2);
-    GameLevel four;  four.Load ("resources/levels/four.lvl",  Width, Height / 2);
-    Levels.push_back(one);
-    Levels.push_back(two);
-    Levels.push_back(three);
-    Levels.push_back(four);
-    Level = 2;
+    ResourceManager::LoadTexture("resources/textures/paddle.png", true, "paddle");
+    // load levels
+    GameLevel one; one.Load("resources/levels/one.lvl", this->Width, this->Height / 2);
+    GameLevel two; two.Load("resources/levels/two.lvl", this->Width, this->Height / 2);
+    GameLevel three; three.Load("resources/levels/three.lvl", this->Width, this->Height / 2);
+    GameLevel four; four.Load("resources/levels/four.lvl", this->Width, this->Height / 2);
+    this->Levels.push_back(one);
+    this->Levels.push_back(two);
+    this->Levels.push_back(three);
+    this->Levels.push_back(four);
+    this->Level = 0;
+    // configure game objects
+    glm::vec2 playerPos = glm::vec2(this->Width / 2.0f - PLAYER_SIZE.x / 2.0f, this->Height - PLAYER_SIZE.y);
+    Player = new GameObject(playerPos, PLAYER_SIZE, ResourceManager::GetTexture("paddle"));
 }
 
 void Game::Update(float dt)
@@ -58,19 +60,35 @@ void Game::Update(float dt)
 
 void Game::ProcessInput(float dt)
 {
+    if(this->State == GAME_ACTIVE)
+    {
+        float velocity = PLAYER_VELOCITY * dt;
+        //move Player
+        if(this->Keys[GLFW_KEY_A])
+        {
+            if(Player->Position.x >= 0.0f)
+                Player->Position.x -= velocity;
+        }
+        if(this->Keys[GLFW_KEY_D])
+        {
+            if(Player->Position.x <= (this->Width - Player->Size.x))
+                Player->Position.x += velocity;
+        }
+    }
    
 }
 
 void Game::Render()
 {
-    if(Game::State == GAME_ACTIVE)
+    if(this->State == GAME_ACTIVE)
     {
         // draw background
         Texture2D texture = ResourceManager::GetTexture("background");
         Renderer->DrawSprite(texture,
-            glm::vec2(0.0f, 0.0f), glm::vec2(Width, Height), 0.0f);
+            glm::vec2(0.0f, 0.0f), glm::vec2(this->Width, this->Height));
         //draw level
-        Levels[Level].Draw(*Renderer);
+        this->Levels[Level].Draw(*Renderer);
+        Player->Draw(*Renderer);
     }
     //Texture2D texture = ResourceManager::GetTexture("face");
     //Renderer->DrawSprite(texture, glm::vec2(200.0f, 200.0f), glm::vec2(300.0f, 400.0f), 45.0f, glm::vec3(0.0f, 1.0f, 0.0f));
--- BreakoutGame/Game.h
+++ BreakoutGame/Game.h
@@ -1,6 +1,8 @@
 #ifndef _GAME_H
 #define _GAME_H
 
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
 #include <vector>
 #include "GameLevel.h"
 
@@ -11,6 +13,10 @@ enum GameState
     GAME_MENU,
     GAME_WIN
 };
+
+// init size and velocity of player paddle
+const glm::vec2 PLAYER_SIZE(100.0f, 20.0f);
+const float PLAYER_VELOCITY(500.0f);
 
 // Game Class
 class Game
